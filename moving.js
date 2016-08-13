b0907a6d572cb9c121bd94488a84865fff59776d
thilinafong <thilinafong@outlook.com>
2016-08-13 22:27:04 +0800

    limit drag

--- moving.js
+++ moving.js
@@ -1,30 +1,55 @@
 $(function(){
-//functions
-function handleMousedown(ev){
-  var $this=$(this);
+(function(){
+  // vars
+  var limitWidth=$('.mask_layer').width();
+  var limitHeight=$('.mask_layer').height();
+  var eleWidth=$('.drag').width();
+  var eleHeight=$('.drag').height();
 
-  var evClientX=ev.clientX;
-  var evClientY=ev.clientY;
-  var eleClientX=$this.get(0).offsetLeft;
-  var eleClientY=$this.get(0).offsetTop;
+  var limitMaxX=limitWidth-eleWidth;
+  var limitMaxY=limitHeight-eleHeight;
+  //functions
+  function handleMousedown(ev){
+    var $this=$(this);
 
-  var ev2eleX=evClientX-eleClientX;
-  var ev2eleY=evClientY-eleClientY;
+    var evClientX=ev.clientX;
+    var evClientY=ev.clientY;
+    var eleClientX=$this.get(0).offsetLeft;
+    var eleClientY=$this.get(0).offsetTop;
 
-  $(document).on('mousemove',function(ev){
-    var moveClientX=ev.clientX;
-    var moveClientY=ev.clientY;
+    var ev2eleX=evClientX-eleClientX;
+    var ev2eleY=evClientY-eleClientY;
 
-    $this.css({top:(moveClientY-ev2eleY),left:(moveClientX-ev2eleX)});
-  });
+    $(document).on('mousemove',function(ev){
+      var moveClientX=ev.clientX;
+      var moveClientY=ev.clientY;
 
-  $(document).on('mouseup',function(ev){
-    // $(document).off();
-    $(document).off('mousemove');
-    $(document).off('mouseup');
-  });
-  return false;
-}
-// =====================================================================
+      var numSetLeft=moveClientX-ev2eleX;
+      var numSetTop=moveClientY-ev2eleY;
+
+      if(numSetLeft<0){
+        numSetLeft=0;
+      }else if(numSetLeft>limitMaxX){
+        numSetLeft=limitMaxX
+      }
+
+      if(numSetTop<0){
+        numSetTop=0;
+      }else if(numSetTop>limitMaxY){
+        numSetTop=limitMaxY
+      }
+
+      $this.css({left:numSetLeft,top:numSetTop});
+    });
+
+    $(document).on('mouseup',function(ev){
+      // $(document).off();
+      $(document).off('mousemove');
+      $(document).off('mouseup');
+    });
+    return false;
+  }
+  // =====================================================================exec
   $('.drag').on('mousedown',handleMousedown);
-});
+})();
+}); //end of ready
